c3235835b5eeda56dec14019745dff2c0574419f
Duncan Mckenna <duncanmckenna@Duncans-MacBook-Pro.local>
2019-11-03 22:09:28 +0000

    isolated JS to only run if element exists

--- src/js/main.js
+++ src/js/main.js
@@ -14,42 +14,44 @@ function toggleClass(el, theClass) {
   }
 }
 
+if (document.querySelector('.accordion')){
 
-const accordionItems = document.querySelectorAll('.accordion'), 
-  accordionContentPanes = document.querySelectorAll('.accordion__body');
+  const accordionItems = document.querySelectorAll('.accordion'), 
+    accordionContentPanes = document.querySelectorAll('.accordion__body');
 
 
-accordionItems.forEach(function(accordion) {
-  const accordionFirstEl = accordion.firstElementChild;
-  accordionFirstEl.addEventListener('click', toggleAccordion);
-  accordionFirstEl.addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-      toggleAccordion(event);
-    }
-  });
-  accordion.style.height = accordion.firstElementChild.offsetHeight + 'px'
-});
-
-function toggleAccordion(event){
-  // loop through each accordion and toggle .active
-  accordionContentPanes.forEach(function(accordion) {
-    var selected = accordion.previousElementSibling === event.target,
-      current = accordion.parentElement.classList.contains('active');
-
-    if (selected && !current) {
-      openAccordion(accordion);
-    } else {
-      closeAccordion(accordion);
-    }
+  accordionItems.forEach(function(accordion) {
+    const accordionFirstEl = accordion.firstElementChild;
+    accordionFirstEl.addEventListener('click', toggleAccordion);
+    accordionFirstEl.addEventListener("keyup", function(event) {
+      event.preventDefault();
+      if (event.keyCode === 13) {
+        toggleAccordion(event);
+      }
+    });
+    accordion.style.height = accordion.firstElementChild.offsetHeight + 'px'
   });
 
-  function closeAccordion(acc){
-    removeClass(acc.parentElement, 'active');
-    acc.parentElement.style.height = acc.previousElementSibling.offsetHeight + 'px';
-  }
-  function openAccordion(acc){
-    addClass(acc.parentElement, 'active');
-    acc.parentElement.style.height = acc.parentElement.offsetHeight + acc.offsetHeight + 10 + 'px';
-  }
+  function toggleAccordion(event){
+    // loop through each accordion and toggle .active
+    accordionContentPanes.forEach(function(accordion) {
+      var selected = accordion.previousElementSibling === event.target,
+        current = accordion.parentElement.classList.contains('active');
+
+      if (selected && !current) {
+        openAccordion(accordion);
+      } else {
+        closeAccordion(accordion);
+      }
+    });
+
+    function closeAccordion(acc){
+      removeClass(acc.parentElement, 'active');
+      acc.parentElement.style.height = acc.previousElementSibling.offsetHeight + 'px';
+    }
+    function openAccordion(acc){
+      addClass(acc.parentElement, 'active');
+      acc.parentElement.style.height = acc.parentElement.offsetHeight + acc.offsetHeight + 10 + 'px';
+    }
+  } 
 }
